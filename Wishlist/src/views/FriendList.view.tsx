94ce68ccb8ffbd4989e2385fb61bee9e6df50f41
enzoc <enzocasa83@gmail.com>
2022-05-22 18:53:25 +0200

    POC presque terminé

--- Wishlist/src/views/FriendList.view.tsx
+++ Wishlist/src/views/FriendList.view.tsx
@@ -9,14 +9,37 @@ import {
     Image,
     KeyboardAvoidingView, TouchableOpacity, ScrollView
 } from "react-native";
-import React from "react";
+import React, {useEffect} from "react";
 import Contact from "../Component/Contact";
 import WishesList from "../Component/WishesList";
 
 
-const FriendListView = ({ route, navigation }): React.ReactElement => {
+const FriendListView = ({ navigation }): React.ReactElement => {
 
-    const {username} = route.params;
+    const [api, setApi] = React.useState({});
+
+    async function getInfosFromApi() {
+        try {
+            const response = await fetch('http://localhost:8080/?url=https%3A%2F%2Fwww.amazon.fr%2FApple-AirPods-Pro-Reconditionn%25C3%25A9%2Fdp%2FB08TJ2LGB8%2Fref%3Dsr_1_8%3Fkeywords%3Dairpods%26qid%3D1653138875%26sr%3D8-84/');
+            const data = await response.json(); // On récupère les infos de l'API.
+            console.log(data);
+
+            setApi(data); // On set les infos de l'épisode dans notre state.
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+
+    useEffect(() => {
+        getInfosFromApi().then(() => {
+            console.log(api)
+        });
+    }, []);
+
+    function test() {
+        console.log(api);
+    }
 
     return (
         <KeyboardAvoidingView
@@ -25,7 +48,9 @@ const FriendListView = ({ route, navigation }): React.ReactElement => {
         >
             <ScrollView>
                 <View style={styles.friendContainer}>
-                    <Text>{username}</Text>
+                    <TouchableOpacity onPress={test}>
+                        <Text>Click Me Boy !</Text>
+                    </TouchableOpacity>
                 </View>
             </ScrollView>
         </KeyboardAvoidingView>
--- Wishlist/src/views/Wishlist.view.tsx
+++ Wishlist/src/views/Wishlist.view.tsx
@@ -6,7 +6,7 @@ import {
 } from "react-native";
 import React, {useEffect} from "react";
 import { db } from "../../firebase/firebase";
-import { query, where, onSnapshot } from "firebase/firestore";
+import {query, where, setDoc, doc, addDoc} from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { collection, getDocs } from "firebase/firestore";
 import WishesList from "../Component/WishesList";
@@ -20,16 +20,67 @@ const WishlistView = ({navigation}): React.ReactElement => {
     // Pop-up formulaire :
     const [isModalVisible, setIsModalVisible] = React.useState(false);
     const [url , setUrl] = React.useState('');
+    const [api, setApi] = React.useState({
+        image: "",
+        price: "",
+        title: "",
+    });
     const auth = getAuth();
 
+
+    async function getInfosFromApi() {
+        try {
+            const encodedUrl = encodeURI(url);
+            console.log("encoded :" + encodedUrl);
+            const response = await fetch(`http://localhost:8080/?url=${encodedUrl}/`);
+            const data = await response.json(); // On récupère les infos de l'API.
+            console.log("DATA : " + data.title);
+            console.log("DATA : " + data.price);
+            console.log("DATA : " + data.image);
+
+            return data;
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+
+    async function setInfosFromApiInBdd(image:string, price:string, name:string) {
+        try {
+            const docRef = await addDoc(collection(db, "wishes"), {
+                image: image,
+                price: price,
+                name: name,
+                uid: auth.currentUser.uid,
+                list: "Birthday"
+            }).then(() => {
+                console.log("Voeu dans la BDD !");
+            }).catch((error) => {
+                console.log(error);
+            });;
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
 
     const popUpModal = () => {
         setIsModalVisible(() => !isModalVisible);
     }
 
-    const addWish = () => {
+    async function addWish() {
         setIsModalVisible(() => !isModalVisible);
-        getWishes();
+
+        try {
+            getInfosFromApi().then((data) => {
+                setInfosFromApiInBdd(data.image, data.price, data.title).then(() => {
+                    getWishes();
+                });
+            });
+        }
+        catch (e) {
+            console.log(e);
+        }
     }
 
 
@@ -48,6 +99,9 @@ const WishlistView = ({navigation}): React.ReactElement => {
         getWishes();
     }, []);
 
+    function test() {
+        console.log(api);
+    }
 
     return (
         <ScrollView style={styles.container}>
@@ -89,6 +143,9 @@ const WishlistView = ({navigation}): React.ReactElement => {
                         </View>
                     </Modal.Container>
                 </Modal>
+                <TouchableOpacity onPress={test}>
+                    <Text>Click Me Boy !</Text>
+                </TouchableOpacity>
             </View>
         </ScrollView>
     )
