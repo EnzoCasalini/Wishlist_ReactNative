7ccf8cde539ceec040e20da1cb675ff743f97f80
enzoc <enzocasa83@gmail.com>
2022-05-23 17:25:21 +0200

    Changement CSS bouton addWish

--- Wishlist/src/Component/WishesList.tsx
+++ Wishlist/src/Component/WishesList.tsx
@@ -10,6 +10,9 @@ import {
     KeyboardAvoidingView, TouchableOpacity
 } from "react-native";
 import React from "react";
+import {collection, deleteDoc, doc, getDocs, query, where} from "firebase/firestore";
+import {db} from "../../firebase/firebase";
+import {getAuth} from "firebase/auth";
 
 interface Wish {
     image: string,
@@ -26,6 +29,19 @@ const WishesList: (props: Wish) => JSX.Element[] = (props: Wish) => {
             return name;
     }
 
+    async function deleteFromBDD() {
+        const collectionRef = collection(db, 'wishes');
+        const q = query(collectionRef, where("image", "==", props.image));
+        const snapshot = await getDocs(q);
+
+        const results = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+
+        results.forEach(async (result) => {
+            const docRef = doc(db, 'wishes', result.id);
+            await deleteDoc(docRef);
+        });
+    };
+
     return (
         <View style={styles.wishContainer} key={props.name}>
             <Image
@@ -37,6 +53,7 @@ const WishesList: (props: Wish) => JSX.Element[] = (props: Wish) => {
             <View style={styles.infoContainer}>
                 <Text style={styles.name}>{checkNameLength(props.name)}</Text>
                 <Text style={styles.description}>{props.price}</Text>
+                <TouchableOpacity onPress={deleteFromBDD}><Text>♥</Text></TouchableOpacity>
             </View>
         </View>
     )
